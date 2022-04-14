19a17573daf222f8b3342a78d1bf519306a2ff01
Felipe Apablaza <felipe.apablaza@protonmail.com>
2022-04-14 15:06:34 -0400

    limpieza de codigo

--- api-cotizaciones/src/cotizacion/cotizacion.controller.ts
+++ api-cotizaciones/src/cotizacion/cotizacion.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { Cliente } from './cliente';
 import { CotizacionService } from './cotizacion.service';
 import { ClienteDto } from './dto/cliente.dto';
@@ -8,7 +8,7 @@ export class CotizacionController {
   constructor(private readonly cotizacionService: CotizacionService) {}
 
   @Post()
-  create(@Body(new ValidationPipe()) dto: ClienteDto) {
+  create(@Body() dto: ClienteDto) {
     const cliente = new Cliente(
       dto.rut,
       new Date(Date.parse(dto.fechaNacimiento)),
